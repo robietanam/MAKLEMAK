92105f826c0da84f10227b02fddd4f8d43f5e681
robietanam <robietanam@gmail.com>
2025-03-29 00:02:03 +0700

    forgor

--- ultralytics/nn/extra_modules/head.py
+++ ultralytics/nn/extra_modules/head.py
@@ -5,12 +5,91 @@ from torch.nn.init import constant_, xavier_uniform_
 
 from ..modules import Conv, DFL, C2f, RepConv
 from .block import *
+from .block import DyDCNv2
 from .rep_block import *
 from .afpn import AFPN_P345, AFPN_P345_Custom, AFPN_P2345, AFPN_P2345_Custom
 from ultralytics.yolo.utils.tal import dist2bbox, make_anchors
 
 __all__ = ['Detect_DyHead', 'Detect_DyHeadWithDCNV3', 'Detect_AFPN_P345', 'Detect_AFPN_P345_Custom', 'Detect_AFPN_P2345', 'Detect_AFPN_P2345_Custom', 'Detect_Efficient', 'DetectAux', 'Detect_TADDH']
 
+
+class Scale(nn.Module):
+    """A learnable scale parameter.
+
+    This layer scales the input by a learnable factor. It multiplies a
+    learnable scale parameter of shape (1,) with input of any shape.
+
+    Args:
+        scale (float): Initial value of scale factor. Default: 1.0
+    """
+
+    def __init__(self, scale: float = 1.0):
+        super().__init__()
+        self.scale = nn.Parameter(torch.tensor(scale, dtype=torch.float))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x * self.scale
+
+class Conv_GN(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.gn = nn.GroupNorm(16, c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.gn(self.conv(x)))
+
+class TaskDecomposition(nn.Module):
+    def __init__(self, feat_channels, stacked_convs, la_down_rate=8):
+        super(TaskDecomposition, self).__init__()
+        self.feat_channels = feat_channels
+        self.stacked_convs = stacked_convs
+        self.in_channels = self.feat_channels * self.stacked_convs
+        self.la_conv1 = nn.Conv2d( self.in_channels,  self.in_channels // la_down_rate, 1)
+        self.relu = nn.ReLU(inplace=True)
+        self.la_conv2 = nn.Conv2d( self.in_channels // la_down_rate,  self.stacked_convs, 1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+        
+        self.reduction_conv = Conv_GN(self.in_channels, self.feat_channels, 1)
+        self.init_weights()
+        
+    def init_weights(self):
+        # self.la_conv1.weight.normal_(std=0.001)
+        # self.la_conv2.weight.normal_(std=0.001)
+        # self.la_conv2.bias.data.zero_()
+        # self.reduction_conv.conv.weight.normal_(std=0.01)
+        
+        torch.nn.init.normal_(self.la_conv1.weight.data, mean=0, std=0.001)
+        torch.nn.init.normal_(self.la_conv2.weight.data, mean=0, std=0.001)
+        torch.nn.init.zeros_(self.la_conv2.bias.data)
+        torch.nn.init.normal_(self.reduction_conv.conv.weight.data, mean=0, std=0.01)
+
+    def forward(self, feat, avg_feat=None):
+        b, c, h, w = feat.shape
+        if avg_feat is None:
+            avg_feat = F.adaptive_avg_pool2d(feat, (1, 1))
+        weight = self.relu(self.la_conv1(avg_feat))
+        weight = self.sigmoid(self.la_conv2(weight))
+
+        # here we first compute the product between layer attention weight and conv weight,
+        # and then compute the convolution between new conv weight and feature map,
+        # in order to save memory and FLOPs.
+        conv_weight = weight.reshape(b, 1, self.stacked_convs, 1) * \
+                          self.reduction_conv.conv.weight.reshape(1, self.feat_channels, self.stacked_convs, self.feat_channels)
+        conv_weight = conv_weight.reshape(b, self.feat_channels, self.in_channels)
+        feat = feat.reshape(b, self.in_channels, h * w)
+        feat = torch.bmm(conv_weight, feat).reshape(b, self.feat_channels, h, w)
+        feat = self.reduction_conv.gn(feat)
+        feat = self.reduction_conv.act(feat)
+
+        return feat
 
 class Detect_TADDH(nn.Module):
     # Task Dynamic Align Detection Head
