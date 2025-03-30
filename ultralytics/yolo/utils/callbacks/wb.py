27b3262ab3d62b14903869e05b97f90201fa760d
robietanam <robietanam@gmail.com>
2025-03-30 13:20:33 +0700

    fix

--- ultralytics/yolo/utils/callbacks/wb.py
+++ ultralytics/yolo/utils/callbacks/wb.py
@@ -23,8 +23,7 @@ def _log_plots(plots, step):
 
 def on_pretrain_routine_start(trainer):
     """Initiate and start project if module is present."""
-    wb.run or wb.init(project=trainer.args.project or 'YOLOv8', name=trainer.args.name, config=vars(trainer.args))
-
+    wb.run or wb.init(project=os.path.basename(trainer.args.project) if trainer.args.project else 'YOLOv8', name=trainer.args.name, config=vars(trainer.args))
 
 def on_fit_epoch_end(trainer):
     """Logs training metrics and model information at the end of an epoch."""
